7bd665bdc3ef715a60a2a3bec4c056d6b04e10ae
BhumilVasoya <bhumil23v@gmail.com>
2024-08-16 08:33:44 +0530

    commit

--- app/src/main/java/com/example/a1/MainActivity.java
+++ app/src/main/java/com/example/a1/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.a1;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -16,6 +17,8 @@ public class MainActivity extends AppCompatActivity {
     EditText username;
     EditText password;
     Button loginButton;
+
+    Intent i;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
